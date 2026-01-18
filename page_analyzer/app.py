c96f8f8320fa17fbcf745c7ae26b42887522af07
grimmlok <Shinigami_626@mail.ru>
2026-01-18 16:32:25 +0300

    Второй шаг

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -4,9 +4,11 @@ from flask import Flask, render_template
 
 load_dotenv()
 
-app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__, template_folder=os.path.join(basedir, '..', 'templates'))
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "please-set-secret")
-  
+
 @app.get("/")
 def index():
     return render_template("index.html")
