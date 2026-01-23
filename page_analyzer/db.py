6b5abbe58b57cdce08172077bbacf8263f901fde
grimmlok <Shinigami_626@mail.ru>
2026-01-23 13:54:49 +0300

    2

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -69,10 +69,9 @@ def add_url_check(url_id, status_code, h1, title, description):
         with conn.cursor() as cur:
             cur.execute("""
                 INSERT INTO url_checks (
-                    url_id, status_code, h1, title, description, 
-                    latest_check_at, latest_status_code
+                    url_id, status_code, h1, title, description
                 ) VALUES (
-                    %s, %s, %s, %s, %s, NOW(), %s
+                    %s, %s, %s, %s, %s
                 )
-            """, (url_id, status_code, h1, title, description, status_code))
+            """, (url_id, status_code, h1, title, description))
             conn.commit()
