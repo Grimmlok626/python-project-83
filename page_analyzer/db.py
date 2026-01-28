3e520e612dc95d3463074985a072b2164ec514bf
grimmlok <Shinigami_626@mail.ru>
2026-01-28 12:38:05 +0300

    41

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -94,11 +94,9 @@ def add_url_check(
         with conn.cursor() as cur:
             if created_at:
                 cur.execute(
-                    """
-                    INSERT INTO url_checks
-                        (url_id, status_code, h1, title, description, created_at)
-                    VALUES (%s, %s, %s, %s, %s, %s)
-                    """,
+                    "INSERT INTO url_checks "
+                    "(url_id, status_code, h1, title, description, created_at) "
+                    "VALUES (%s, %s, %s, %s, %s, %s)",
                     (url_id, status_code, h1, title, description, created_at),
                 )
             else:
